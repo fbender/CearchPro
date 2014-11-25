c1584a457d4b2500000ce1fd96c842f11eb0fbd0
Steffen <mohring@feedbackmedia.de>
2014-11-25 18:06:33 +0100

    Added "Search Global" setting, possibility of multiple page roots to search for

---
+++ languages/en/tl_module.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Contao Open Source CMS
+ *
+ * Copyright (C) 2005-2014 Leo Feyer
+ *
+ * @package
+ * @author    Steffen Mohring
+ * @license   LGPL
+ * @copyright Steffen Mohring 2014
+ */
+
+$GLOBALS['TL_LANG']['tl_module']['cearchpro_legend'] = 'Cearch Pro';
+$GLOBALS['TL_LANG']['tl_module']['levensthein'][0] = 'Use Cearch Pro';
+$GLOBALS['TL_LANG']['tl_module']['levensthein'][1] = '';
+$GLOBALS['TL_LANG']['tl_module']['maxDist'][0] = 'Max Distance';
+$GLOBALS['TL_LANG']['tl_module']['maxDist'][1] = '';
+$GLOBALS['TL_LANG']['tl_module']['search_global'][0] = 'Language independant seach';
+$GLOBALS['TL_LANG']['tl_module']['search_global'][1] = 'Searches in all available languages, not only the one which is currently visited';
